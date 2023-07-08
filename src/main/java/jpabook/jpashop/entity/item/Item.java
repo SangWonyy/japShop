eb90e41522d6c7a4eb7c3ad09ab227c1ca43cb9b
sangwonyou <tkddnjs231@naver.com>
2023-07-08 16:57:45 +0900

    feat: category 반대로한거 수정

--- src/main/java/jpabook/jpashop/entity/item/Item.java
+++ src/main/java/jpabook/jpashop/entity/item/Item.java
@@ -22,17 +22,7 @@ public abstract class Item {
     private int price;
     private int stockQuantity;
 
-    @ManyToMany
-    @JoinTable(name = "category_item",
-            joinColumns = @JoinColumn(name = "category_id"),
-            inverseJoinColumns = @JoinColumn(name = "item_id")
-    )
+
+    @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
-
-    @ManyToOne
-    @JoinColumn(name = "parent_id")
-    private Category parent;
-
-    @OneToMany(mappedBy = "parent")
-    private List<Category> child = new ArrayList<>();
 }
